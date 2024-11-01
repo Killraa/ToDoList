437f52037e93869497b3ccb61ea5cadc9bcabf16
HSKR4963 <HSKR4963@L02780>
2024-11-01 08:55:14 +0100

    Add project files.

---
+++ ToDoList/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine("Hello, World I am a programmer! Or soon i will be :-)");
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+string userChoice = Console.ReadLine();
+Console.WriteLine("user input:" + userChoice);
+userChoice = "ABC";
+
+Console.ReadKey();
